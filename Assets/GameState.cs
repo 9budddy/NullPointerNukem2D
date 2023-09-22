cf7ca7513e1032f0593d0992ce3fed055e59426a
Budddy <budddyjellz@gmail.com>
2023-09-22 14:36:06 -0400

    Game State and Logic

---
+++ Assets/GameState.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "gamestate", menuName = "State/JBGameState")]
+public class GameState : ScriptableObject
+{
+
+    public int browiePoints { get; set; }
+    public int points { get; set; }
+
+}
